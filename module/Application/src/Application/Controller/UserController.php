8236a5caacb03111260e2a9a7eeae32ddd473d9c
Naj Haq <scrapeyard@gmail.com>
2015-04-21 21:14:24 -0400

    Removed un-necessary methods from Controller

--- module/Application/src/Application/Controller/UserController.php
+++ module/Application/src/Application/Controller/UserController.php
@@ -11,72 +11,10 @@ namespace Application\Controller;
 use Zend\EventManager\EventManagerInterface;
 use Zend\Mvc\Controller\AbstractRestfulController;
 use Zend\View\Model\JsonModel;
-use Zend\View\Model\ViewModel;
 
 
 class UserController extends AbstractRestfulController
 {
-    protected $collectionsMethods = array('GET', 'POST');
-    protected $itemMethods = array('GET', 'PUT', 'DELETE');
-
-
-    protected function getOptions()
-    {
-        if ($this->params()->fromRoute('id', false)) {
-            return $this->itemMethods;
-        }
-        return $this->collectionsMethods;
-    }
-
-    protected $viewModelMap = array();
-    public function options()
-    {
-
-
-        $response = $this->getResponse();
-
-        $response->getHeaders()->addHeaderLine('Allow', implode($this->getOptions(), ','));
-        return $response;
-    }
-
-    public function setEventManager(EventManagerInterface $event)
-    {
-        $this->events = $event;
-        $event->attach('dispatch', array($this, 'checkOptions'), 10);
-    }
-
-    public function checkOptions($e)
-    {
-        $matches  = $e->getRouteMatch();
-        $response = $e->getResponse();
-        $request  = $e->getRequest();
-        $method   = $request->getMethod();
-
-        // test if we matched an individual resource, and then test
-        // if we allow the particular request method
-        if ($matches->getParam('id', false)) {
-            if (!in_array($method, $this->resourceMethods)) {
-                $response->setStatusCode(405);
-                return $response;
-            }
-            return;
-        }
-
-        // We matched a collection; test if we allow the particular request
-        // method
-        if (!in_array($method, $this->collectionsMethods)) {
-            $response->setStatusCode(405);
-            return $response;
-        }
-
-//        if (in_array($this->getRequest()->getMethod(), $this->getOptions())) {
-//            return ;
-//        }
-//        $response = $this->getResponse();
-//        $response->setStatusCode(405);
-//        return $response;
-    }
-
     public function get($id)
     {
         echo "Getting $id";
